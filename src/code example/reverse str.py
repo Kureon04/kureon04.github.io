93b66b4f76988ffd78b76f29c34106b0080cedf0
Kureon04 <95969595+Kureon04@users.noreply.github.com>
2022-02-24 21:35:51 +0100

    Delete src directory

--- src/code example/reverse str.py
+++
@@ -1,7 +0,0 @@
-original = str(input("Write something: "))
-inverse = ''
-i = 1
-while i < len(original) + 1:
-    inverse  = inverse  + original[-i]
-    i = i + 1
-print("inverted string: " + inverse)
